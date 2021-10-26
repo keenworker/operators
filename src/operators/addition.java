f753d5ce5b8ce725e3ad6760d7d09aaaf923a474
keenworker <dassjn.005@gmail.com>
2021-10-26 18:05:50 +0530

    simple commit

---
+++ src/operators/addition.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class addition {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a = 7/4.0f * 9/2.0f;
+		System.out.println(a);
+	}
+	
+}
